78ea4d81c3b0c07ba21c371eb86935bba3808fd3
Yash Avsarmal <businessspaces2@gmail.com>
2024-10-20 13:42:44 +0530

    our team removed
    Co-Authored-By: Yash Avsarmal <134092522+yashavsarmal30@users.noreply.github.com>

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       <Hero />
       <Features />
       <About />
-      <Team />
+      {/*<Team />*/}
       <Portfolio />
       <Testimonials />
       <Brands />
