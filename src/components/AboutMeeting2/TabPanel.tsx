21336462a5b4b6d32b3635da856aac62df39b626
Yash Avsarmal <134092522+yashavsarmal30@users.noreply.github.com>
2025-01-16 13:03:24 +0530

    updated-tab-panel

--- src/components/AboutMeeting2/TabPanel.tsx
+++ src/components/AboutMeeting2/TabPanel.tsx
@@ -16,11 +16,11 @@ export default function TabPanel({
       <div className="w-full px-4 lg:w-1/2">
         <div className="relative z-30 mb-14 h-[490px] max-w-[600px] lg:mb-0">
           <div className="absolute left-0 top-0 aspect-[86/121] w-full max-w-[344px]">
-            <img src={image1} alt={`${image1Alt || "image alt text"}`} fill />
+            <Image src={image1} alt={`${image1Alt || "image alt text"}`} fill />
           </div>
 
           <div className="absolute right-0 top-1/2 z-10 aspect-[53/66] w-full max-w-[315px] -translate-y-1/2">
-            <img src={image2} alt={`${image2Alt || "image alt text"}`} fill />
+            <Image src={image2} alt={`${image2Alt || "image alt text"}`} fill />
 
             <div className="absolute -left-5 -top-5 -z-10 h-full w-full border border-primary border-opacity-10 bg-primary bg-opacity-5 backdrop-blur-[6px] dark:border-white dark:border-opacity-10 dark:bg-white dark:bg-opacity-10"></div>
           </div>
