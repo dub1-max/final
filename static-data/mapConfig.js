f1b78713373fb3a6a36a8d7db559ac454ad4ab28
Yash Avsarmal <134092522+yashavsarmal30@users.noreply.github.com>
2025-01-17 23:53:25 +0530

    del

--- static-data/mapConfig.js
+++
@@ -1,8 +0,0 @@
-const mapConfig = {
-    googleMapUrl1: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.554100745289!2d55.34305767603425!3d25.251939329479967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5d2b5f33ac7f%3A0xe59c3bea5bf9fa90!2sConnecting%20Minds%20Business%20Center%20LLC!5e0!3m2!1sen!2sin!4v1728818825985!5m2!1sen!2sin",
-    googleMapUrl2: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.554100745289!2d55.34305767603425!3d25.251939329479967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5d2b5f33ac7f%3A0xe59c3bea5bf9fa90!2sConnecting%20Minds%20Business%20Center%20LLC!5e0!3m2!1sen!2sin!4v1728818825985!5m2!1sen!2sin",
-    googleMapUrl3: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.554100745289!2d55.34305767603425!3d25.251939329479967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f5d2b5f33ac7f%3A0xe59c3bea5bf9fa90!2sConnecting%20Minds%20Business%20Center%20LLC!5e0!3m2!1sen!2sin!4v1728818825985!5m2!1sen!2sin"
-
-};
-
-export default mapConfig;
