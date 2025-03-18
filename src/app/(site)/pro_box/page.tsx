a1bfe3ef946a34c0d96dd617c258a92a154e3583
SHUBHAM3000-beep <therealshubhamjadhav1@gmail.com>
2025-03-18 20:46:06 +0530

    no change

--- src/app/(site)/pro_box/page.tsx
+++ src/app/(site)/pro_box/page.tsx
@@ -9,41 +9,33 @@ import mapConfig from "@/static-data/mapConfig";
 import Maps from "@/components/Maps";
 import Carousel_UI from "@/components/ui/carousel";
 
-
-
 const siteName = process.env.SITE_NAME;
-const slides = [
-  "https://i.ibb.co/ncrXc2V/1.png",
-  "https://i.ibb.co/B3s7v4h/2.png",
-  "https://i.ibb.co/XXR8kzF/3.png"
-  ];
+const slideData = [
+  {
+    title: "Mystic Mountains",
+    button: "Explore Component",
+    src: "https://ucaa6706fc452e14920f58e7e9fb.previews.dropboxusercontent.com/p/thumb/ACl25JCMIAd-CrE5Blk3EdXQeuIYh4cjK_S32HlTs2GWeJR3acGc3QJvYQJBcu-m8WpvSmYFRUzQxwmEtZjvlrFpTg-GtZ9i4EDq4dgdqKmZl6BdPIrFhUpO0UAKBnjwflWq4z5oB8wTmJjmfht870B6GZEk_g79IloeyRk4TFY07G8Lj1XwI6I4lIQCCl6w-n_ZEPSdjqzqDWmLHUT_IeUq3JH29ZlguwMZ7tGRQgDcW7rcJMwiq4ZX9u1puLAhV7X_o0jRR8v79hqIBFPktweNeNm0x9vtm7QrmmykOqa2QpTLhwVBzTqNIbddD5ItIAfIw4q-PflKoTwDFTxBiUpgTaFB8qYStY4rAWTtk5cnl81QtlYESSkXju9C7Wd-jH9TEw8V-pIV5Q_-_u5wd0k6UlNZaDLq0YyZzqzrlx4IUx-5fTj4l1BstF_t1k_SLjuvffyy16GvGxQI2YO0zAktq7Jyr3Tr0yoOY98Ryifyajh2n2MSmtc7d4agVNg46WUvvwh1WMyXiO2-mlXfms19/p.jpeg",
+  },
+  {
+    title: "Urban Dreams",
+    button: "Explore Component",
+    src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Neon Nights",
+    button: "Explore Component",
+    src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    title: "Desert Whispers",
+    button: "Explore Component",
+    src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
 
-  const slideData = [
-    {
-      title: "Mystic Mountains",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Urban Dreams",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Neon Nights",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      title: "Desert Whispers",
-      button: "Explore Component",
-      src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-  ];
 export const metadata: Metadata = {
   title: `Pro Box | ${siteName}`,
   description: "This is Support page",
-  // other metadata
 };
 
 export default function ProBoxPage() {
@@ -51,13 +43,12 @@ export default function ProBoxPage() {
     <>
       <section className="pt-10">
         <div className="px-4 xl:container">
-          {/* <CarouselProBox autoSlide={true} autoSlideInterval={6000} slides={slides} /> */}
           <Carousel_UI slides={slideData} />
-          <AboutPB/>
+          <AboutPB />
           <Pro_Box />
-          <Maps googleMapUrl={mapConfig.connectingmindsurl}/>
+          <Maps googleMapUrl={mapConfig.connectingmindsurl} />
           <Calendar />
-          <Support/>
+          <Support />
         </div>
       </section>
     </>
