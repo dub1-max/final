7f0b853b3cb6e5739dbe72650a669f6cdca52b5d
eyecruit <natural.f00dst0r3s@gmail.com>
2025-04-14 02:01:32 +0530

    test

--- src/app/(site)/apple_carousel/page.tsx
+++ src/app/(site)/apple_carousel/page.tsx
@@ -66,7 +66,7 @@ const data = [
   {
     category: "Business Centers",
     title: "Connecting Minds",
-    src: "https://uc0f9d0e27987502d3ee64a8e143.previews.dropboxusercontent.com/p/thumb/ACly-GRPFyuGcw74WUf4S-JgDakvQKJScYApsAec1HsI_Tf4o8r7sFhyNuejZ0ByzBVeaVclas_Pbl7Y2QBm9dYUWC3V4xKh-vNoQzv6lk0sA2N5ir-UOi_0qYwKCHNRjlNbhnp8_njYAczFY4DcdBWQHamqwZtfg8XmzhF5PtO5ktNhYSWNXtWD5LQtE2THHsv-DJAu_MJtrB2dyhec-HqO1usvxF6_Kpw9-VF6eFr0_sxf6VVdfzwxQpEvCzhwjYWAsG_PbCUBeczqniEsg31v6isAnAY1DPCrxd-V8gml2gsN0e82mA_KjKMLj-BxPxBOMvYrLQpIYQd7AwxQvAHtqyBx-JTJPGqolNTZs7qQttoLfpqkuBvdHh-43OHwca2SLi-6GNjyyQEWSpgmzJGAXUvgTXDjprevm0h5RU0ivAm8yk19DTjE9GfaFhMMxJk0QLTv6l05T97rfvm-evK-u5fI72R1VMeGhpdYBLX3vB2SG0XD7nXuueytXYjOekUGpYRInsmIPpAdmwXjdAWH/p.jpeg",
+    src: "https://i.postimg.cc/0Qc2d5Th/G-QP6d.jpg",
     content: null,
   },
 ];
